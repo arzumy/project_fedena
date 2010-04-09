786b0109a4d7ef94ada9b6a2e50cd5b594d2aba3
Arzumy MD <hello@ar.zu.my>
2010-04-10 00:39:28 +0800

    load pdf-writer through config.gem

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,12 @@
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
-require "pdf/writer"
-require "pdf/simpletable"
+
 Rails::Initializer.run do |config|
-  config.gem "declarative_authorization"
-  config.gem 'prawn', :version=>'~>0.8.4'
+  config.gem "declarative_authorization", :version => '~>0.4.1'
+  config.gem 'prawn',                     :version => '~>0.8.4'
+  config.gem 'pdf-writer',                :version => '~>1.1.8', :lib => 'pdf/writer'
+  config.gem 'pdf-writer',                :version => '~>1.1.8', :lib => 'pdf/simpletable'
 
   config.time_zone = 'UTC'
 end
