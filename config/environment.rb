e93b8755562feed34b070796fefb227cfc5166d0
Arzumy MD <hello@ar.zu.my>
2010-04-09 23:48:48 +0800

    use rails 2.3.5 and cleaned up environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,11 @@
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+
 require File.join(File.dirname(__FILE__), 'boot')
 require "pdf/writer"
 require "pdf/simpletable"
 Rails::Initializer.run do |config|
-  config.gem "declarative_authorization", :source => "http://gemcutter.org"
-  config.time_zone = 'UTC'
-  config.gem 'prawn', :version=>'0.6.3'
+  config.gem "declarative_authorization"
+  config.gem 'prawn', :version=>'~>0.8.4'
 
-  config.action_controller.session = {
-    :session_key => '_fedena_session',
-    :secret      => '414909bfa58bf168d2ffde2ddad6afad5d3b7fca8025598337b55efa2e665d263993792f78fe346c15c1ea5f4b08fd7696de24c7968d55b2d198348478944f0b'
-  }
-  config.load_once_paths += %W( #{RAILS_ROOT}/lib )
+  config.time_zone = 'UTC'
 end
