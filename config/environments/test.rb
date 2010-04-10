dc57bd5af8753e8fe68ddf94ea609fe2651b1074
Arzumy MD <hello@ar.zu.my>
2010-04-11 00:32:10 +0800

    basic setup so we could run test. yay!

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,3 +26,8 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
+
+config.gem 'turn',          :version => '~> 0.7.0'
+config.gem 'shoulda',       :version => '~> 2.10.2'
+config.gem 'factory_girl',  :version => '~> 1.2.3', :lib => false
+config.gem 'rr',            :version => '~> 0.10.11'
